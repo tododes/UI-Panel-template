8ff967b9e0a1fc0022d5e0f536507c10bf04bfd0
tododes <tododes3@gmail.com>
2017-07-26 22:48:39 +0700

    Initial commit

---
+++ Panel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PanelStatus { ZERO, VISIBLE, INFLATE_DEFFLATE }
+
+public class Panel : MonoBehaviour {
+
+    [SerializeField] private int desiredSize;
+    private int inflateIndex;
+    private RectTransform rect;
+    private Vector3 inflateRate;
+    private PanelManager manager;
+
+    public PanelStatus status;
+    public Panel prev;
+    public List<Panel> next = new List<Panel>();
+
+    void Start(){
+        manager = PanelManager.singleton;
+        rect = GetComponent<RectTransform>();
+        inflateRate = new Vector3(desiredSize * 1.5f, desiredSize * 1.5f, 0);
+    }
+
+    public void startInflate(){
+        inflateIndex = 1;
+        status = PanelStatus.INFLATE_DEFFLATE;
+    }
+
+    public void startDeflate(){
+        inflateIndex = 0;
+        status = PanelStatus.INFLATE_DEFFLATE;
+    }
+
+    void FixedUpdate() {
+        if(inflateIndex == 1){
+            if (rect.localScale.x < desiredSize)
+                rect.localScale += inflateRate * Time.deltaTime;
+            else {
+                status = PanelStatus.VISIBLE;
+            }
+        }
+        else if (inflateIndex == 0){
+            if (rect.localScale.x > 0f)
+                rect.localScale -= inflateRate * Time.deltaTime;
+            else {
+                status = PanelStatus.ZERO;
+            }
+        }
+    }
+
+    public void GoToNext(int index) {
+        startDeflate();
+        StartCoroutine(DelayToNextPanel(next[index]));
+    }
+
+    public void GoToPrev() {
+        startDeflate();
+        StartCoroutine(DelayToNextPanel(prev));
+    }
+
+    public float getInflateTime(){
+        return desiredSize / inflateRate.x;
+    }
+
+    private IEnumerator DelayToNextPanel(Panel next){
+        yield return new WaitForSeconds(getInflateTime() + 1f);
+        manager.setCurrentPanel(next);
+    }
+
+}
---
+++ PanelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour {
+
+    public static PanelManager singleton;
+
+    [SerializeField] private Panel current;
+
+    void Awake(){
+        singleton = this;
+    }
+
+	void Start () {
+        current.startInflate();
+	}
+
+	void Update () {
+		
+	}
+
+    public void setCurrentPanel(Panel p) {
+        current = p;
+        current.startInflate();
+    }
+}
